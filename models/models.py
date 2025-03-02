92cdb1032429adc42ca677436cabb749bcaaee29
SuJoDev <petrzaporozhets985@gmail.com>
2025-03-02 21:34:48 +0500

    Update models.py

--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import select
+from sqlalchemy.orm import DeclarativeBase, Mapped,  mapped_column
+from sqlalchemy import ForeignKey, JSON
+
 
 class Base(DeclarativeBase):
     pass
@@ -7,6 +8,26 @@ class Base(DeclarativeBase):
 class UsersModel(Base):
     __tablename__ = "Users"
     
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key= True, autoincrement= True)
     username: Mapped[str] = mapped_column()
     password: Mapped[str] = mapped_column()
+    
+class DocumentsModel(Base):
+    __tablename__ = "Documents"
+    
+    id:Mapped[int] = mapped_column(primary_key=True, autoincrement= True)
+    title: Mapped[str] = mapped_column()
+    date_created: Mapped[str] = mapped_column()
+    date_updated: Mapped[str] = mapped_column()
+    category: Mapped[str] = mapped_column()
+    has_coment: Mapped[bool] = mapped_column()
+    
+class ComentsModel(Base):
+    __tablename__ = "Coments"
+    
+    id: Mapped[int] = mapped_column(primary_key= True, autoincrement= True)
+    title: Mapped[str] = mapped_column()
+    date_created: Mapped[str] = mapped_column()
+    date_updated: Mapped[str] = mapped_column()
+    author: Mapped[JSON] = mapped_column(JSON)
+    document_id: Mapped[int] = mapped_column(ForeignKey("Documents.id"))
